0b4882e087d67c54274269ef56dbf5e3d96f36ff
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-31 22:40:37 -0300

    a

---
+++ FiapCoreX/FiapCoreX/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FiapCoreX.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+
+namespace FiapCoreX.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
