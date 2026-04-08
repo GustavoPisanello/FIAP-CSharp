e14ad2ca342eb3fb83c163548fd3c159cf0efb06
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-07 22:46:58 -0300

    Projetinho

---
+++ Fiap.Banco/Fiap.Banco.API/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fiap.Banco.API.Models
+{
+    public class Cliente
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [ForeignKey("bancoId")]
+        public Banco? Banco { get; set; }
+    }
+}
